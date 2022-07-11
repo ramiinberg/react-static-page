a12b69515b6bdfa6f47c56a7481d5375aa404884
ramiinberg <rami.inberg@gmail.com>
2022-07-11 11:37:33 +0300

    Added header and footer to component

--- src/App.js
+++ src/App.js
@@ -3,13 +3,24 @@ import './App.css';
 
 function Page() {
   return (
-    <ul className='bold-text'>
-    <li>Was first released in 2013</li>
-    <li>Was originally created by Jordan Walk</li>
-    <li>Has well over 100K stars on Github</li>
-    <li>Is maintained by Facebook</li>
-    <li>Powers thousands of enterprise apps, including mobile apps</li>
-  </ul> 
+    <>
+    <header>
+      <nav>
+        <img src={logo} alt='React JS logo' className='img' />
+      </nav>
+    </header>
+    <h1>Reasons I'm excited to learn React</h1>
+    <ol className='bold-text'>
+      <li>Was first released in 2013</li>
+      <li>Was originally created by Jordan Walk</li>
+      <li>Has well over 100K stars on Github</li>
+      <li>Is maintained by Facebook</li>
+      <li>Powers thousands of enterprise apps, including mobile apps</li>
+    </ol> 
+    <footer>
+      <p>© 20xx Shrimberg development. All rights reserved.</p>
+    </footer>
+  </>
   )
 }
 
