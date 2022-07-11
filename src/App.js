abfaf16429d25261cf36c8cf7666207a9263f7c4
ramiinberg <rami.inberg@gmail.com>
2022-07-11 11:00:01 +0300

    showing a logo and facts of react

--- src/App.js
+++ src/App.js
@@ -4,20 +4,15 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <img src={logo} className='img' alt='React.js logo' />
+      <h1>Fun facts about react</h1>
+      <ul className='bold-text'>
+        <li>Was first released in 2013</li>
+        <li>Was originally created by Jordan Walk</li>
+        <li>Has well over 100K stars on Github</li>
+        <li>Is maintained by Facebook</li>
+        <li>Powers thousands of enterprise apps, including mobile apps</li>
+      </ul>
     </div>
   );
 }
