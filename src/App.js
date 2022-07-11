ff1aad093040611a8a8f71f7113804b3c32a8768
ramiinberg <rami.inberg@gmail.com>
2022-07-11 11:32:58 +0300

    Making first custom component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,23 @@
 import logo from './logo.svg';
 import './App.css';
 
+function Page() {
+  return (
+    <ul className='bold-text'>
+    <li>Was first released in 2013</li>
+    <li>Was originally created by Jordan Walk</li>
+    <li>Has well over 100K stars on Github</li>
+    <li>Is maintained by Facebook</li>
+    <li>Powers thousands of enterprise apps, including mobile apps</li>
+  </ul> 
+  )
+}
+
 function App() {
   return (
     <div className="App">
-      <img src={logo} className='img' alt='React.js logo' />
+      <Page />
+      {/* <img src={logo} className='img' alt='React.js logo' />
       <h1>Fun facts about react</h1>
       <ul className='bold-text'>
         <li>Was first released in 2013</li>
@@ -12,7 +25,8 @@ function App() {
         <li>Has well over 100K stars on Github</li>
         <li>Is maintained by Facebook</li>
         <li>Powers thousands of enterprise apps, including mobile apps</li>
-      </ul>
+      </ul> 
+      */}
     </div>
   );
 }
