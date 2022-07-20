7ad290cf4cacd9e850809e201ce15d22929ce689
ramiinberg <rami.inberg@gmail.com>
2022-07-20 12:48:48 +0300

    dark and white mode to application

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,19 @@
+import React from 'react'
 import Main from './components/Main'
 import Navbar from './components/Navbar'
 import './app.css'
 
 function App() {
+  const [darkMode, setDarkMode] = React.useState(false)
+
+  const toggleDarkMode = () => {
+    setDarkMode(dark => !dark)
+  }
+
   return (
     <div className='container'>
-      <Navbar />
-      <Main />
+      <Navbar darkMode={darkMode} toggleDarkMode={toggleDarkMode} />
+      <Main darkMode={darkMode} />
     </div>
   )
 }
