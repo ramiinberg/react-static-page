c9290e5644d75a7b7bc0d859d2da53c9f49fd2d5
ramiinberg <rami.inberg@gmail.com>
2022-07-20 11:15:57 +0300

    fixed navbar styling

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,14 +2,32 @@ import './navbar.css'
 import logo from '../images/react-icon-small.png'
 
 function Navbar(props) {
-  const { darkMode } = props
+  const { darkMode, toggleDarkMode } = props
+
+  const handleKeyDown = e => {
+    if (e.key === 'Enter') {
+      console.log('switch')
+    }
+  }
+
   return (
     <nav className={darkMode ? 'dark' : ''}>
-      <div className='nav-logo'>
-        <img className='react-logo' src={logo} alt='React js logo' />
-        <div className='react-title'>ReactFacts</div>
+      <img className='nav-logo-icon' src={logo} alt='React js logo' />
+      <h3 className='nav-logo-title'>ReactFacts</h3>
+      <div className='toggler'>
+        <p className='toggler-light'>Light</p>
+        <div
+          className='toggler-slider'
+          onClick={toggleDarkMode}
+          onKeyDown={handleKeyDown}
+          aria-checked={darkMode}
+          role='switch'
+          tabIndex={0}
+        >
+          <div className='toggler-slider-circle' />
+        </div>
+        <p className='toggler-dark'>Dark</p>
       </div>
-      <div className='nav-links'>React Course - Project 1</div>
     </nav>
   )
 }
